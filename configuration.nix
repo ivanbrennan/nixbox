8389431250f38dc8ef9fcf46cb98a01974040f71
ivanbrennan <ivan.brennan@gmail.com>
2017-11-23 01:00:42 -0500

    programs.bash.enableCompletion

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,7 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.bash.enableCompletion = true;
+  programs.bash.enableCompletion = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
