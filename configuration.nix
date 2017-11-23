2a349dedc33571d8244ae34e2a6968c6d5f4a2aa
ivanbrennan <ivan.brennan@gmail.com>
2017-11-23 01:04:16 -0500

    system.autoUpgrade.enable
    This causes a systemd timem to automatically run
    
        nixos-rebuild switch --upgrade
    
    once a day.

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,8 @@
   nix.gc.automatic = true;
   nix.gc.dates = "03:15";
 
+  system.autoUpgrade.enable = true;
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
