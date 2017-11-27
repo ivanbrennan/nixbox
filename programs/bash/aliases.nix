1d53823f568073926c9b21cb6acf0de7cc387959
ivanbrennan <ivan.brennan@gmail.com>
2017-11-26 19:53:12 -0500

    fix bash aliases

--- programs/bash/aliases.nix
+++ programs/bash/aliases.nix
@@ -1,12 +1,15 @@
 {
-  fgg = "fg '%-'";
-  gst = "git status";
-  gpr = "git log origin/master.. --reverse";
-  gpp = "git log origin/master.. --reverse -p";
-  gpc = "git log origin/master.. -- | wc -l";
-  grep = "grep --color=auto";
-  la = "ls -A";
   SS = "unset __ETC_BASHRC_SOURCED && . /etc/bashrc";
+  fgg = "fg '%-'";
+  gpc = "git log origin/master.. -- | wc -l";
+  gpp = "git log origin/master.. --reverse -p";
+  gpr = "git log origin/master.. --reverse";
+  grep = "grep --color=auto";
+  gst = "git status";
+  l = "ls -Alh";
+  la = "ls -A";
+  ll = "ls -l";
+  ls = "ls --color=tty";
   t = "tmux";
   tls = "tmux ls";
 }
