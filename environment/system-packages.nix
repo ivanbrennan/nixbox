1f817db077e83d8e75461d1f693b1a55443e4aa0
ivanbrennan <ivan.brennan@gmail.com>
2018-01-16 23:18:31 -0500

    more system-packages

--- environment/system-packages.nix
+++ environment/system-packages.nix
@@ -2,16 +2,32 @@ pkgs:
 
 let
   packages = with pkgs; [
+    abcde
     ag
+    alacritty
+    aspell
+    aspellDicts.en
     chromium
+    conky
     dmidecode
     docker
+    emacs
+    flac
     git
+    global
+    lshw
     mkpasswd
+    nix-repl
     par
+    pstree
     slack
+    sqliteInteractive
+    stack
     tmux
     tree
+    universal-ctags
+    vlc
+    weechat
     xcape
     xclip
   ];
