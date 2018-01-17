3c6b02b554c2c9fd277cf3872546fd1af923d3c8
ivanbrennan <ivan.brennan@gmail.com>
2018-01-16 22:37:16 -0500

    ./machines/nixosbox/default.nix boot attributes

--- configuration.nix
+++ configuration.nix
@@ -14,18 +14,6 @@
       ./users/default.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.initrd.luks.devices = [
-    {
-      name = "root";
-      device = "/dev/disk/by-uuid/01f0c2eb-300c-49e9-9b5f-0180f45445b0";
-      preLVM = true;
-    }
-  ];
-
   # Select internationalisation properties.
   i18n = {
     consoleKeyMap = "us";
