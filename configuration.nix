d9bcb5ba9bb0e4aa90fe7e1d7f85e4a24f2b4813
ivanbrennan <ivan.brennan@gmail.com>
2018-02-13 22:04:37 -0500

    disable autoUpgrade
    This never actually ran, because I tend to close the laptop at night.
    More importantly, I'd prefer to run upgrades manually with:
    
      sudo nix-channel --update
      sudo nixos-rebuild

--- configuration.nix
+++ configuration.nix
@@ -37,8 +37,6 @@
   nix.gc.automatic = true;
   nix.gc.dates = "03:15";
 
-  system.autoUpgrade.enable = true;
-
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
