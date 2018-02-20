3ed96adb1e93ba3c492f4d831bb41f2304c62611
ivanbrennan <ivan.brennan@gmail.com>
2018-02-19 22:59:51 -0500

    /etc/git{attributes,config,ignore}
    Move much of my git configuration into system-wide etc/

--- environment/default.nix
+++ environment/default.nix
@@ -5,6 +5,9 @@
     systemPackages = (import ./system-packages.nix) pkgs;
     variables = import ./variables.nix;
     etc = {
+      "gitattributes".source = pkgs.lib.mkForce ./etc/gitattributes;
+      "gitconfig".source = pkgs.lib.mkForce ./etc/gitconfig;
+      "gitignore".source = pkgs.lib.mkForce ./etc/gitignore;
       "inputrc".source = pkgs.lib.mkForce ./etc/inputrc;
       "sysless".source = pkgs.sysless + "/etc/sysless";
     };
