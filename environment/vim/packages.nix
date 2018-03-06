274edc07edeb1bd23090cb5544e7ed09bba786ae
ivanbrennan <ivan.brennan@gmail.com>
2018-03-05 23:59:15 -0500

    vim-unimpaired

--- environment/vim/packages.nix
+++ environment/vim/packages.nix
@@ -1,19 +1,33 @@
 pkgs:
 
 let
-  customPlugins.coot = pkgs.vimUtils.buildVimPlugin {
-    name = "coot";
-    src = pkgs.fetchFromGitHub {
-      owner = "ivanbrennan";
-      repo = "coot";
-      rev = "dbba4c34a20e1cebcd54e62bec0de455f5615adb";
-      sha256 = "14h0r5hkwx0r5rhzc56sdnkdczzy2nrv8xhyj2hsgwbyaa3a5j16";
+  customPlugins = {
+    coot = pkgs.vimUtils.buildVimPlugin {
+      name = "coot";
+      src = pkgs.fetchFromGitHub {
+        owner = "ivanbrennan";
+        repo = "coot";
+        rev = "dbba4c34a20e1cebcd54e62bec0de455f5615adb";
+        sha256 = "14h0r5hkwx0r5rhzc56sdnkdczzy2nrv8xhyj2hsgwbyaa3a5j16";
+      };
+    };
+    vim-unimpaired = pkgs.vimUtils.buildVimPlugin {
+      name = "vim-unimpaired";
+      src = pkgs.fetchFromGitHub {
+        owner = "tpope";
+        repo = "vim-unimpaired";
+        rev = "c77939c4aff30b2ed68deb1752400ec15f17c3a2";
+        sha256 = "0qd9as008r2vycls48bfb163rp7dddw7l495xn4l1gl00sh79cxy";
+      };
     };
   };
   vim = pkgs.vim_configurable.customize {
     name = "vim";
     vimrcConfig.customRC = import ./vimrc;
     vimrcConfig.vam.knownPlugins = pkgs.vimPlugins // customPlugins;
-    vimrcConfig.vam.pluginDictionaries = [ { name = "coot"; } ];
+    vimrcConfig.vam.pluginDictionaries =
+      [ { name = "coot"; }
+        { name = "vim-unimpaired"; }
+      ];
   };
 in [ vim ]
