472e6cff96e8e7dd3f63c634af84f894f435813e
ivanbrennan <ivan.brennan@gmail.com>
2018-03-29 00:08:58 -0400

    overlays: take 3
    I decided to go back to defining a single overlay and leaving the
    package derivations alone (aside from the earlier cleanup of their
    builder scripts).

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,8 +3,7 @@
       stdenv = self.stdenv;
       fetchFromGitHub = self.fetchFromGitHub;
     };
-  })
-  (self: super: {
+
     linuxPackages = (super.linuxPackagesFor super.linuxPackages.kernel).extend (
       inner_self: inner_super: {
         i8042_debounce = super.callPackage ./i8042-debounce {
@@ -14,15 +13,13 @@
         };
       }
     );
-  })
-  (self: super: {
+
     sysless = super.callPackage ./sysless {
       stdenv = self.stdenv;
       fetchFromGitHub = self.fetchFromGitHub;
       less = self.less;
     };
-  })
-  (self: super: {
+
     vln = super.callPackage ./vln {
       stdenv = self.stdenv;
       fetchFromGitHub = self.fetchFromGitHub;
