9276c89b8c213f0635cc496caf04bc01f82f70af
ivanbrennan <ivan.brennan@gmail.com>
2018-04-03 21:48:50 -0400

    FZF_DEFAULT_OPTS: exact and colors
    It's really nice to see that setting colors here affects fzf colors
    inside Vim as well.

--- environment/variables.nix
+++ environment/variables.nix
@@ -4,5 +4,16 @@
   VISUAL = "vim";
   GIT_EDITOR = "vim";
   GIT_MERGE_AUTOEDIT = "no";
-  FZF_DEFAULT_OPTS = "--exact";
+  FZF_DEFAULT_OPTS = ''
+    --exact
+    --color=fg+:#c4d5e5
+    --color=bg+:#222222
+    --color=hl+:#f8bb39
+    --color=hl:#26a6a6
+    --color=info:#48778b
+    --color=marker:#bbe068
+    --color=pointer:#222222
+    --color=prompt:#181818
+    --color=spinner:#181818
+  '';
 }
