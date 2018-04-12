365b576fef0304d266db02dc95d5b07f24987d93
ivanbrennan <ivan.brennan@gmail.com>
2018-04-12 00:41:00 -0400

    FZF_DEFAULT_OPTS
    Still need to figure out the preview window.

--- environment/variables.nix
+++ environment/variables.nix
@@ -7,6 +7,12 @@
   FZF_DEFAULT_COMMAND = "fd -t file -E GTAGS -E GRTAGS -E GPATH";
   FZF_CTRL_T_COMMAND = "$FZF_DEFAULT_COMMAND";
   FZF_DEFAULT_OPTS = ''
+    --prompt='• '
+    --history=$HOME/.fzf_history
+    --bind=ctrl-p:up
+    --bind=ctrl-n:down
+    --bind=alt-p:previous-history
+    --bind=alt-n:next-history
     --exact
     --color=fg+:#c4d5e5
     --color=bg+:#222222
@@ -15,7 +21,7 @@
     --color=info:#48778b
     --color=marker:#bbe068
     --color=pointer:#222222
-    --color=prompt:#181818
+    --color=prompt:#b8e068
     --color=spinner:#181818
   '';
 }
