87b64bdbb714a3063afe3645b8994d78f3d14d79
ivanbrennan <ivan.brennan@gmail.com>
2018-04-25 23:17:08 -0400

    overlays: fzf.overrideAttrs (newer version)
    This is total overkill in order to pick up a recent fix in fzf.
      https://github.com/junegunn/fzf/issues/1268
    
    I'm committing it mostly in order to demonstrate how overrideAttrs can
    be used in an overlay, in this case to pick up a more recent version.
    The fix this particular update picks up is not worth the overhead of
    maintaining another overlay, so I'll actually be reverting this commit.
    I can live without this particular fix until a new release of fzf makes
    it into nixpkgs.

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,4 +13,15 @@
   })
 
   (import ./vim-overlay)
+
+  (self: super: {
+    fzf = super.fzf.overrideAttrs (oldAttrs: rec {
+      src = super.fetchFromGitHub {
+        owner = "junegunn";
+        repo = "fzf";
+        rev = "6eac4af7db54b2b736a2ede928b14456c0496711";
+        sha256 = "0mskwh231c40cqia1y5cy02m52a3grk5d72bcir5idxrj55ip6vz";
+      };
+    });
+  })
 ]
