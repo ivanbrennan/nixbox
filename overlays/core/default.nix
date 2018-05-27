10b60d54ffe45df886b5d19d86ead3024fb17ad4
ivanbrennan <ivan.brennan@gmail.com>
2018-05-27 01:14:59 -0400

    update channel, pickup update to lastpass-cli
    The lastpass-cli version has been updated in the nixpkgs tree, so I can
    stop rolling my own. During the rebuild, a warning stated that
    system.stateVersion has been renamed to system.nixos.stateVersion.

--- overlays/core/default.nix
+++ overlays/core/default.nix
@@ -3,18 +3,6 @@ self: super: {
 
   interactive-editor = super.callPackage ./interactive-editor { };
 
-  # get certificate fix
-  # https://github.com/lastpass/lastpass-cli/pull/410
-  lastpass-cli = super.lastpass-cli.overrideAttrs (oldAttrs: rec {
-    version = "1.3.1";
-    src = super.fetchFromGitHub {
-      owner = "lastpass";
-      repo = "lastpass-cli";
-      rev = "v${version}";
-      sha256 = "11drzmfdvb8ydw1dxaz9zz8rk0jjqmfv076vydz05qqvgx59s38h";
-    };
-  });
-
   linuxPackages = (super.linuxPackagesFor super.linuxPackages.kernel).extend (
     self': super': {
       i8042_debounce = super'.callPackage ./i8042-debounce { };
