6a2f47880858b5c7b7e6bd3c943b1809192142f8
ivanbrennan <ivan.brennan@gmail.com>
2018-05-28 20:01:18 -0400

    sumall-env
    Provide a manifest of the packages used in my sumall environment. This
    way I can install/uninstall with a single command:
    
      nix-env --install --attr nixos.sumall-env \
        -I ssh-auth-sock=/tmp/hax \
        -I ssh-config-file=/dev/null

--- overlays/sumall/default.nix
+++ overlays/sumall/default.nix
@@ -2,4 +2,12 @@ self: super: {
   sumall-cli = super.callPackage ./sumall-cli { pkgs = self; };
 
   sumall-completion = super.callPackage ./sumall-completion { pkgs = self; };
+
+  sumall-env = super.buildEnv {
+    name = "sumall-env";
+    paths = with self; [
+      sumall-cli
+      sumall-completion
+    ];
+  };
 }
