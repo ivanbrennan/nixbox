6ab49e0e31455dade173027e37166f8310af83ff
ivanbrennan <ivan.brennan@gmail.com>
2018-06-23 23:34:02 -0400

    pia
    Configure PIA openvpn servers. I got the configuration started as
    follows:
    
      wget -P src https://www.privateinternetaccess.com/openvpn/openvpn.zip
      unzip src/openvpn.zip -d src
    
      mkdir pia
      cp src/*.{crt,pem} pia
      touch pia/server.nix
    
      account=<PIA_ACCOUNT>
      cat > pia/login.private <<EOF
      ${account}
      $(pass show pia/${account} | head -1)
      EOF
      chmod 400 pia/login.private
    
      mkdir pia/{ca,nl}
      sed 's/auth-user-pass/& login.private/' 'src/CA Montreal.ovpn' > pia/ca/remote.private
      sed 's/auth-user-pass/& login.private/' 'src/Netherlands.ovpn' > pia/nl/remote.private
    
      default_nix="\
      let
        server = import ../server.nix;
      in
        server { remote = builtins.readFile ./remote.private; }
      "
    
      for dest in pia/*; do
        [ -d "$dest" ] &&
          printf "$default_nix" > ${dest}/default.nix
      done
    
      sudo chown --recursive root:root pia
      sudo mv pia /etc/nixos/services/openvpn/
    
      unset account default_nix dest

--- services/openvpn/default.nix
+++ services/openvpn/default.nix
@@ -1,6 +1,8 @@
 {
   servers = {
-    # systemctl {start|stop} openvpn-sumall.service
+    # systemctl {start|stop} openvpn-{attr}.service
+    pia-ca = import ./pia/ca;
+    pia-nl = import ./pia/nl;
     sumall = import ./sumall;
   };
 }
