150bbf1210ba44939120f67fda9fb21d8c546f7d
ivanbrennan <ivan.brennan@gmail.com>
2018-07-13 22:36:30 -0400

    clean up bash prompt initialization
    Refactor for readability and to avoid leaking color variables all over
    the place.

--- programs/bash/prompt.sh
+++ programs/bash/prompt.sh
@@ -1,24 +1,45 @@
 # Provide a nice prompt if the terminal supports it.
-if [ "$TERM" != "dumb" -o -n "$INSIDE_EMACS" ]; then
-  GREEN="\033[0;32m"
-  BLACK="\033[0;30m"
-  BOLD="\033[1m"
-  NORMAL="\033[0m"
 
-  line1="╭\[${BOLD}\]\w\[${NORMAL}\]\$(_git_ps1_)\[${NORMAL}\] \[${BLACK}\]\t\[${NORMAL}\]"
-  line2="╰(\u)• "
-  PS1="\n${line1}\n${line2}"
-  PS2=" ❯ "
-  PS4=" + "
+_build_prompt() {
+    if [ "$TERM" = "dumb" ] && [ -z "$INSIDE_EMACS" ]; then
+        return
+    fi
 
-  _git_ps1_() {
-    local color="${GREEN}"
+    if [ "$TERM" = "xterm" ]; then
+        _build_basic_prompt
+    else
+        _build_color_prompt
+    fi
+}
+
+_build_basic_prompt() {
+    PS1="\[\033]2;\h:\u:\w\007\]${PS1}"
+}
+
+_build_color_prompt() {
+    local green black bold normal line1 line2
+
+    green="\033[0;32m"
+    black="\033[0;30m"
+    bold="\033[1m"
+    normal="\033[0m"
+
+    line1="╭\[${bold}\]\w\[${normal}\]\$(_git_ps1_)\[${normal}\] \[${black}\]\t\[${normal}\]"
+    line2="╰(\u)• "
+
+    PS1="\n${line1}\n${line2}"
+    PS2=" ❯ "
+    PS4=" + "
+}
+
+_git_ps1_() {
+    local green="\033[0;32m"
+    local normal="\033[0m"
 
     # __git_ps1 inserts the current git branch where %s is
-    echo "$(__git_ps1 " (${color}%s${NORMAL})")"
-  }
+    echo "$(__git_ps1 " (${green}%s${normal})")"
+}
 
-  if test "$TERM" = "xterm"; then
-    PS1="\[\033]2;\h:\u:\w\007\]${PS1}"
-  fi
-fi
+_build_prompt
+
+unset _build_prompt _build_basic_prompt _build_color_prompt
