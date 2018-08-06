abe76dff750246dda1c2bfebeefa81f0ccda11cd
ivanbrennan <ivan.brennan@gmail.com>
2018-08-06 08:51:06 -0400

    netrc: api.github.com

--- environment/default.nix
+++ environment/default.nix
@@ -1,18 +1,25 @@
 { pkgs, ... }:
 
-{
+let
+  githubUsername = import ./github-username.private;
+  githubToken = import ./github-token.private;
+in {
   environment = {
     systemPackages = (import ./system-packages.nix) pkgs;
     variables = (import ./variables.nix) pkgs;
     etc = {
       "gitattributes".source = "${pkgs.etcdots}/etc/gitattributes";
-      "gitconfig".source = pkgs.lib.mkForce ./etc/gitconfig;
+      "gitconfig".source = pkgs.lib.mkForce etc/gitconfig;
       "gitignore".source = "${pkgs.etcdots}/etc/gitignore";
       "inputrc".source = "${pkgs.etcdots}/etc/inputrc";
       "nix/netrc".source = pkgs.writeText "netrc" ''
         machine github.com
-        login ${import ./github-username.private}
-        password ${import ./github-token.private}
+        login ${githubUsername}
+        password ${githubToken}
+
+        machine api.github.com
+        login ${githubUsername}
+        password ${githubToken}
       '';
     };
   };
