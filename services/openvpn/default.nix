121b0f60261774c4bec0c5a3e687bde02a1d94ae
ivanbrennan <ivan.brennan@gmail.com>
2019-03-23 18:30:59 -0400

    add odeko vpn

--- services/openvpn/default.nix
+++ services/openvpn/default.nix
@@ -3,6 +3,7 @@
     # systemctl {start|stop} openvpn-{attr}.service
     pia-ca = import ./pia/ca;
     pia-nl = import ./pia/nl;
+    odeko  = import ./odeko;
     sumall = import ./sumall;
   };
 }
