7bf619a4adff26952afd726ccd57f09ff3b885dd
ivanbrennan <ivan.brennan@gmail.com>
2019-03-23 18:41:55 -0400

    update interactive_editor dependencies

--- overlays/core/interactive-editor/gemset.nix
+++ overlays/core/interactive-editor/gemset.nix
@@ -1,14 +1,18 @@
 {
   ffi = {
+    groups = ["default"];
+    platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "0zw6pbyvmj8wafdc7l5h7w20zkp1vbr2805ql5d941g2b20pk4zr";
+      sha256 = "0j8pzj8raxbir5w5k6s7a042sb5k02pg0f8s4na1r5lan901j00p";
       type = "gem";
     };
-    version = "1.9.23";
+    version = "1.10.0";
   };
   interactive_editor = {
     dependencies = ["spoon"];
+    groups = ["default"];
+    platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
       sha256 = "0g7mll7k1ccam22z4hcdi7j5nc86ls3i8s1dhvwkmwz75nj934bm";
@@ -18,6 +22,8 @@
   };
   spoon = {
     dependencies = ["ffi"];
+    groups = ["default"];
+    platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
       sha256 = "1x5kp9dzq9lvsg81xjlpagsiw3qnnsb2wnq8qdwp6vykyql8xl9s";
