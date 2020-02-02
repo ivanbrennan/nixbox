825a7179e20e386631ae706022fc4ad4a283df15
ivanbrennan <ivan.brennan@gmail.com>
2020-02-01 23:29:38 -0500

    programs.gnupg.agent.enableSSHSupport
    To add an ssh-key to ~/.gnupg/sshcontrol
    
      ssh-add /path/to/private/key
    
    It will prompt you for the key's passphrase, then prompt for a
    passphrase to encrypt it with. The result is that a "keygrip" for the
    key is added to ~/.gnupg/sshcontrol so you won't have to add it again.

--- programs/default.nix
+++ programs/default.nix
@@ -3,6 +3,10 @@
 {
   programs = {
     bash = (import ./bash) pkgs;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
     less = (import ./less) pkgs;
     ssh  = (import ./ssh);
     tmux = (import ./tmux) pkgs;
