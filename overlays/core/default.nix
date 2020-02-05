3aeb72b8b6cbaa4c1ab90d52a1407dc2412edb1a
ivanbrennan <ivan.brennan@gmail.com>
2020-02-05 08:20:40 -0500

    Revert "tab2meta"
    This reverts commit 935d53a6c20227ef37154621cb32f4a0ed651bff.

--- overlays/core/default.nix
+++ overlays/core/default.nix
@@ -46,7 +46,6 @@ self: super: {
         sha256 = "0w58z0b16hr52sjynkpqd1v090sgyd5glwc8wb248j5l8q2b50zv";
       };
     });
-    tab2meta = super.callPackage ./tab2meta { };
   };
 
   vln = super.callPackage ./vln { };
--- services/default.nix
+++ services/default.nix
@@ -4,7 +4,10 @@
   services = {
     emacs.enable = true;
 
-    interception-tools = (import ./interception-tools) pkgs;
+    interception-tools = {
+      enable = true;
+      udevmonConfig = ./udevmon.yaml;
+    };
 
     openvpn = import ./openvpn;
 
