49eb3c83b4b2a2175917bf459697787aa78dbdaa
ivanbrennan <ivan.brennan@gmail.com>
2020-02-16 14:06:32 -0500

    brightness control with light

--- programs/default.nix
+++ programs/default.nix
@@ -4,6 +4,7 @@
   programs = {
     bash = (import ./bash) pkgs;
     less = (import ./less) pkgs;
+    light.enable = true;
     ssh  = (import ./ssh);
     tmux = (import ./tmux) pkgs;
   };
--- users/default.nix
+++ users/default.nix
@@ -9,6 +9,7 @@
         "docker"
         "networkmanager"
         "vboxusers"
+        "video"
         "wheel"
       ];
       hashedPassword = "$6$4uOYQEuFA$RqNmGNfQcR6mPK2.jSHPntF43HgN6BJP4nwQANNUbp8ulpquniQNqecgUMVGRBsjBzt2b7gJBhCCedbUYmI/60";
