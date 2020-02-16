e55b5c0b5f63e2d25f107157edceb644232aaadf
ivanbrennan <ivan.brennan@gmail.com>
2020-02-15 20:59:15 -0500

    compton

--- services/default.nix
+++ services/default.nix
@@ -19,5 +19,15 @@
     xbanish.enable = true;
 
     xserver = (import ./xserver) pkgs;
+
+    compton = {
+      enable = true;
+      backend = "glx";
+      vSync = true;
+      settings = {
+        glx-no-stencil = true;
+        paint-on-overlay = true;
+      };
+    };
   };
 }
