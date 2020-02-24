c966234dd84bc2a594f231048bad8bc62a06e99a
ivanbrennan <ivan.brennan@gmail.com>
2020-02-23 19:47:36 -0500

    xmonad: no-op whitespace

--- services/xserver/xmonad/xmonad.hs
+++ services/xserver/xmonad/xmonad.hs
@@ -5,64 +5,63 @@ import System.Exit
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
-
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-  -- launch/kill
-  [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-  , ((modm,               xK_space ), spawn "dmenu_run -fn monospace:size=12 -l 16 -i -nb '#1c1c1c' -nf '#a5adb7' -sb '#1f1f1f' -sf '#c8f5ff'")
-  , ((modm .|. shiftMask, xK_d     ), kill)
+    -- launch/kill
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((modm,               xK_space ), spawn "dmenu_run -fn monospace:size=12 -l 16 -i -nb '#1c1c1c' -nf '#a5adb7' -sb '#1f1f1f' -sf '#c8f5ff'")
+    , ((modm .|. shiftMask, xK_d     ), kill)
 
-  -- layout algorithms
-  , ((modm .|. shiftMask, xK_space ), sendMessage NextLayout)
+    -- layout algorithms
+    , ((modm .|. shiftMask, xK_space ), sendMessage NextLayout)
 
-  -- focus
-  , ((modm,               xK_Tab   ), windows W.focusDown)
-  , ((modm,               xK_j     ), windows W.focusDown)
-  , ((modm,               xK_k     ), windows W.focusUp  )
-  , ((modm,               xK_m     ), windows W.focusMaster)
+    -- focus
+    , ((modm,               xK_Tab   ), windows W.focusDown)
+    , ((modm,               xK_j     ), windows W.focusDown)
+    , ((modm,               xK_k     ), windows W.focusUp  )
+    , ((modm,               xK_m     ), windows W.focusMaster)
 
-  -- swap
-  , ((modm,               xK_Return), windows W.swapMaster)
-  , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-  , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
+    -- swap
+    , ((modm,               xK_Return), windows W.swapMaster)
+    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
+    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
 
-  -- resize
-  , ((modm,               xK_h     ), sendMessage Shrink)
-  , ((modm,               xK_l     ), sendMessage Expand)
+    -- resize
+    , ((modm,               xK_h     ), sendMessage Shrink)
+    , ((modm,               xK_l     ), sendMessage Expand)
 
-  -- tile
-  , ((modm,               xK_t     ), withFocused $ windows . W.sink)
+    -- tile
+    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
 
-  -- increment/decrement master area
-  , ((modm .|. shiftMask, xK_comma ), sendMessage (IncMasterN 1))
-  , ((modm .|. shiftMask, xK_period), sendMessage (IncMasterN (-1)))
+    -- increment/decrement master area
+    , ((modm .|. shiftMask, xK_comma ), sendMessage (IncMasterN 1))
+    , ((modm .|. shiftMask, xK_period), sendMessage (IncMasterN (-1)))
 
-  -- Toggle the status bar gap
-  -- Use this binding with avoidStruts from Hooks.ManageDocks.
-  -- See also the statusBar function from Hooks.DynamicLog.
-  --
-  -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
+    -- Toggle the status bar gap
+    -- Use this binding with avoidStruts from Hooks.ManageDocks.
+    -- See also the statusBar function from Hooks.DynamicLog.
+    --
+    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
-  -- quit or restart
-  , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-  , ((modm              , xK_q     ), spawn "xmonad --recompile && xmonad --restart")
-  ]
-  ++
-  -- mod-[1..9], Switch to workspace N
-  -- mod-shift-[1..9], Move client to workspace N
-  [ ((m .|. modm, k), windows $ f i)
-    | (i, k) <- zip (XMonad.workspaces conf) [xK_1..xK_9]
-    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-  ]
-  ++
-  -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-  -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-  --
-  [ ((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-    | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
-  ]
+    -- quit or restart
+    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    , ((modm              , xK_q     ), spawn "xmonad --recompile && xmonad --restart")
+    ]
+    ++
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    [ ((m .|. modm, k), windows $ f i)
+      | (i, k) <- zip (XMonad.workspaces conf) [xK_1..xK_9]
+      , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    ]
+    ++
+    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+    --
+    [ ((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+      | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+      , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+    ]
 
 ------------------------------------------------------------------------
 -- Window rules:
@@ -80,9 +79,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 -- 'className' and 'resource' are used below.
 --
 myManageHook = composeAll
-  [ className =? "vlc"            --> doFloat
-  , resource  =? "desktop_window" --> doIgnore
-  ]
+    [ className =? "vlc"            --> doFloat
+    , resource  =? "desktop_window" --> doIgnore
+    ]
 
 ------------------------------------------------------------------------
 -- Event handling
@@ -121,13 +120,13 @@ myStartupHook = return ()
 main = xmonad defaults
 
 defaults = def
-  { terminal           = "alacritty"
-  , clickJustFocuses   = False
-  , normalBorderColor  = "gray13" -- "#212121"
-  , focusedBorderColor = "gray29" -- "#4A4A4A"
-  , keys               = myKeys
-  , manageHook         = myManageHook
-  , handleEventHook    = myEventHook
-  , logHook            = myLogHook
-  , startupHook        = myStartupHook
-  }
+    { terminal           = "alacritty"
+    , clickJustFocuses   = False
+    , normalBorderColor  = "gray13" -- "#212121"
+    , focusedBorderColor = "gray29" -- "#4A4A4A"
+    , keys               = myKeys
+    , manageHook         = myManageHook
+    , handleEventHook    = myEventHook
+    , logHook            = myLogHook
+    , startupHook        = myStartupHook
+    }
