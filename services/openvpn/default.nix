10377db607013ad9d283fc9180f3a5f9d6f33ad1
ivanbrennan <ivan.brennan@gmail.com>
2020-03-22 16:46:30 -0400

    pia-swiss

--- services/openvpn/default.nix
+++ services/openvpn/default.nix
@@ -1,9 +1,10 @@
 {
   servers = {
     # systemctl {start|stop} openvpn-{attr}.service
-    pia-ca = import ./pia/ca;
-    pia-nl = import ./pia/nl;
-    odeko  = import ./odeko;
-    sumall = import ./sumall;
+    pia-ca    = import ./pia/ca;
+    pia-nl    = import ./pia/nl;
+    pia-swiss = import ./pia/swiss;
+    odeko     = import ./odeko;
+    sumall    = import ./sumall;
   };
 }
