4170de30e38d3de1470de07c7e86ea7d52a7eed2
ivanbrennan <ivan.brennan@gmail.com>
2020-07-20 08:45:37 -0400

    unpin vim
    I originally pinned this because a bug had been introduced that broke
    quickfix window detection. That's since been fixed.

--- overlays/vim/default.nix
+++ overlays/vim/default.nix
@@ -13,15 +13,7 @@ in
 
   vimPrivatePlugins = (import ./plugins.nix) super;
 
-  vim-configured = (self.vim_configurable.overrideAttrs (old: rec {
-    version = "8.1.1549";
-    src = super.fetchFromGitHub {
-      owner = "vim";
-      repo = "vim";
-      rev = "v${version}";
-      sha256 = "1jvj127ff962d6hb1l2vid8c13ychqc6w22rp1pbrqf7i4b5xwf1";
-    };
-  })).customize {
+  vim-configured = self.vim_configurable.customize {
     name = "vim";
     vimrcConfig = configured;
   };
