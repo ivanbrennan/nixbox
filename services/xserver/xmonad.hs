cf098d1280e8ea3e61fedbc306098cc3ad26eb1b
ivanbrennan <ivan.brennan@gmail.com>
2020-09-04 23:10:45 -0400

    xmonad: simpler workspace cycle Alt Tab
    Allow the NSP workspace to be included in this cycle. Though I usually
    want to filter this out, there are occasions when I'd like to be able to
    view the NSP workspace. Continue to exclude it from the Alt-period and
    Alt-comma cycles, but let Alt-Tab include it.

--- services/xserver/xmonad.hs
+++ services/xserver/xmonad.hs
@@ -26,9 +26,9 @@ import Data.Default (def)
 import Graphics.X11
   ( Button, KeyMask, KeySym, Window, button1, controlMask, mod4Mask, noModMask, shiftMask,
     xK_1, xK_9, xK_Alt_L, xK_Alt_R, xK_Down, xK_Left, xK_Print, xK_Right, xK_Tab, xK_Up,
-    xK_a, xK_c, xK_comma, xK_d, xK_e, xK_g, xK_grave, xK_h, xK_i, xK_j, xK_k, xK_l, xK_m,
-    xK_n, xK_p, xK_period, xK_q, xK_r, xK_semicolon, xK_slash, xK_space, xK_t, xK_u, xK_v,
-    xK_w, xK_x, xK_z,
+    xK_a, xK_c, xK_comma, xK_d, xK_e, xK_g, xK_h, xK_i, xK_j, xK_k, xK_l, xK_m, xK_n, xK_p,
+    xK_period, xK_q, xK_r, xK_semicolon, xK_slash, xK_space, xK_t, xK_u, xK_v, xK_w, xK_x,
+    xK_z,
   )
 import Graphics.X11.ExtraTypes
   ( xF86XK_AudioLowerVolume, xF86XK_AudioMute, xF86XK_AudioRaiseVolume, xF86XK_Copy,
@@ -55,8 +55,7 @@ import XMonad.StackSet
 import qualified XMonad.StackSet as W
 
 {- xmonad-contrib -}
-import XMonad.Actions.CycleRecentWS (cycleWindowSets)
-import XMonad.Actions.CycleWS (Direction1D (Next, Prev), WSType (WSIs), moveTo)
+import XMonad.Actions.CycleWS (Direction1D (Next, Prev), WSType (WSIs, NonEmptyWS), moveTo)
 import XMonad.Actions.DynamicWorkspaces (addHiddenWorkspace)
 import XMonad.Actions.FlexibleResize (mouseResizeEdgeWindow)
 import XMonad.Actions.Submap (submap)
@@ -139,7 +138,7 @@ keys' conf@(XConfig {modMask}) =
         toggleRecentWS
       ),
       ( (modMask, xK_Tab),
-        cycleWindowSets recentWS [xK_Alt_L, xK_Alt_R] xK_Tab xK_grave
+        moveTo Next NonEmptyWS
       ),
       -- sublayouts
       ( (mod4Mask .|. shiftMask, xK_h),
