6760f5a33e42d5b3b3bb1c0f5c64de4139b64375
ivanbrennan <ivan.brennan@gmail.com>
2020-12-16 08:59:23 -0500

    zathurarc

--- environment/default.nix
+++ environment/default.nix
@@ -24,6 +24,7 @@ in {
       "xdg/gtk-3.0/settings.ini".source = etc/xdg/gtk-3.0/settings.ini;
       "xdg/gtk-2.0/gtkrc".source = etc/xdg/gtk-2.0/gtkrc;
       "xdg/kitty/kitty.conf".source = etc/xdg/kitty/kitty.conf;
+      "zathurarc".source = etc/zathurarc;
     };
     extraInit = ''
       export XDG_CONFIG_DIRS="/etc/xdg''${XDG_CONFIG_DIRS:+:}$XDG_CONFIG_DIRS"
