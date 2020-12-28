ccff7e9f02581a5acbcdadead0c7c6f9c929d071
ivanbrennan <ivan.brennan@gmail.com>
2020-12-28 16:29:16 -0500

    gnome: remove trackers, setup, remote desktop

--- without-desktop-manager.nix
+++ without-desktop-manager.nix
@@ -26,8 +26,6 @@ in
   services.gnome3.gnome-keyring.enable = true;
   services.gnome3.gnome-online-accounts.enable = true;
   services.gnome3.gnome-online-miners.enable = true;
-  services.gnome3.tracker-miners.enable = true;
-  services.gnome3.tracker.enable = true;
   services.hardware.bolt.enable = true;
   services.packagekit.enable = true;
   services.udisks2.enable = true;
@@ -58,8 +56,6 @@ in
   services.colord.enable = true;
   services.gnome3.chrome-gnome-shell.enable = true;
   services.gnome3.glib-networking.enable = true;
-  services.gnome3.gnome-initial-setup.enable = true;
-  services.gnome3.gnome-remote-desktop.enable = true;
   services.gnome3.gnome-settings-daemon.enable = true;
   services.gnome3.gnome-user-share.enable = true;
   services.gnome3.rygel.enable = true;
@@ -94,13 +90,6 @@ in
     isAllowed = true;
     isSystem = true;
   };
-
-  fonts.fonts = with pkgs; [
-    cantarell-fonts
-    dejavu_fonts
-    source-code-pro # Default monospace font in 3.32
-    source-sans-pro
-  ];
 
   programs.seahorse.enable = true;
 
