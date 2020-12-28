4c5d5b0548ab297eedb4e97a75be8bdac28f10be
ivanbrennan <ivan.brennan@gmail.com>
2020-12-28 16:40:17 -0500

    gnome: remove evolution, shell, docs, control center

--- environment/system-packages.nix
+++ environment/system-packages.nix
@@ -111,14 +111,8 @@ with pkgs; [
   gnome3.gnome-backgrounds
   gnome3.gnome-bluetooth
   gnome3.gnome-color-manager
-  gnome3.gnome-control-center
-  gnome3.gnome-getting-started-docs
-  gnome3.gnome-shell
-  gnome3.gnome-shell-extensions
-  gnome3.gnome-themes-extra
   nixos-artwork.wallpapers.simple-dark-gray
   nixos-artwork.wallpapers.simple-dark-gray-bottom
-  gnome-user-docs
   glib # for gsettings
   gnome-menus
   gtk3.out # for gtk-launch
