e70862e42888d9a83cca8764aa98d8fb8594dead
ivanbrennan <ivan.brennan@gmail.com>
2020-12-30 22:17:01 -0500

    clean up screencast script

--- overlays/core/screencast/default.nix
+++ overlays/core/screencast/default.nix
@@ -1,83 +1,21 @@
-{ coreutils
-, ffmpeg
+{ ffmpeg
 , gnugrep
 , libnotify
+, runCommandLocal
 , slop
-, writeShellScriptBin
+, stdenv
 , xrandr
 }:
 
-writeShellScriptBin "screencast" ''
-  set -u
+runCommandLocal "screencast" { } ''
+  install -D -m755 ${./screencast} $out/bin/$name
+  patchShebangs --host $out/bin
+  substituteInPlace $out/bin/$name                         \
+      --replace "ffmpeg"      "${ffmpeg}/bin/ffmpeg"         \
+      --replace "grep"        "${gnugrep}/bin/grep"          \
+      --replace "notify-send" "${libnotify}/bin/notify-send" \
+      --replace "slop"        "${slop}/bin/slop"             \
+      --replace "xrandr"      "${xrandr}/bin/xrandr"
 
-  expectedExitCode=0
-  checkError() {
-      local exitCode=$?
-      if [ "$exitCode" -eq "$expectedExitCode" ]
-      then
-          expectedExitCode=0
-      else
-          exit "$exitCode"
-      fi
-  }
-  trap checkError ERR
-
-  screenRegion() {
-      ${xrandr}/bin/xrandr \
-          | ${gnugrep}/bin/grep \
-                --max-count=1 \
-                --only-matching \
-                --extended-regexp \
-                '[0-9]+x[0-9]+\+[0-9]+\+[0-9]+' \
-          | tr x+ ' '
-  }
-
-  selectRegion() {
-      ${slop}/bin/slop --format "%w %h %x %y"
-  }
-
-  exec > ~/.screencast.log 2>&1
-
-  all=false
-  while getopts "a" opt
-  do
-      case "$opt" in
-          a) all=true ;;
-          *) exit 1   ;;
-      esac
-  done
-
-  tmpfile="$(mktemp -t screencast-XXXXXXX.mkv)"
-  palette="$(mktemp -t palette-XXXXXXX.png)"
-  trap "rm -f '$tmpfile' '$palette'" EXIT QUIT TERM
-
-  mkdir -p "$HOME/Pictures"
-  output="$HOME/Pictures/screencast.$(${coreutils}/bin/date +%s)"
-
-  if "$all"
-  then
-      region=$(screenRegion)
-  else
-      region=$(selectRegion)
-  fi
-
-  read -r W H X Y < <(echo "$region")
-
-  # Recording may be stopped by running "killall ffmpeg",
-  # in which case ffmpeg exits with code 255.
-  #
-  expectedExitCode=255
-  ${ffmpeg}/bin/ffmpeg -y -f x11grab -s "$W"x"$H" -i :0.0+"$X","$Y" "$tmpfile"
-  expectedExitCode=0
-
-  ${libnotify}/bin/notify-send 'generating palette'
-  ${ffmpeg}/bin/ffmpeg -y -i "$tmpfile" -vf fps=10,palettegen "$palette"
-
-  ${libnotify}/bin/notify-send 'generating gif'
-  ${ffmpeg}/bin/ffmpeg -y -i "$tmpfile" -i "$palette" -filter_complex "paletteuse" "$output".gif
-
-  mv "$tmpfile" "$output".mkv
-  echo "$output"
-
-  ${libnotify}/bin/notify-send "size $(du -h "$output".gif | cut --fields=1)"
+  ${stdenv.shell} -n $out/bin/$name
 ''
