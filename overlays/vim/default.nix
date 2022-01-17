1bd98d98e276b7bd415278bd28ad498edb87eb22
ivanbrennan <ivan.brennan@gmail.com>
2022-01-17 14:24:18 -0500

    vim: compile with clipboard support
    https://github.com/NixOS/nixpkgs/pull/117022
    https://github.com/NixOS/nixpkgs/issues/26726

--- overlays/vim/default.nix
+++ overlays/vim/default.nix
@@ -13,6 +13,12 @@ in
 
   vimPrivatePlugins = (import ./plugins.nix) super;
 
+  vim_configurable = super.vim_configurable.overrideAttrs (old: {
+    # Make the X Toolkit Intrinsics library (libXt) available during the build
+    # so that Vim will compile itself with clipboard support.
+    buildInputs = old.buildInputs ++ [ self.xorg.libXt ];
+  });
+
   vim-configured = self.vim_configurable.customize {
     name = "vim";
     vimrcConfig = configured;
