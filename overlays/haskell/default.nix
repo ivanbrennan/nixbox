fc9bb6797ffb95a7576757b6471a0d14d171489d
ivanbrennan <ivan.brennan@gmail.com>
2022-01-22 14:33:00 -0500

    xmobar: 0.37 -> 0.41

--- overlays/haskell/default.nix
+++ overlays/haskell/default.nix
@@ -1,25 +1,6 @@
 self: super:
 {
   haskellPackages = super.haskellPackages.override {
-    overrides = hself: hsuper: hsuper // {
-      # TODO: Check whether this override is still necessary.
-      xmobar =
-        super.haskell.lib.dontHaddock
-        ( hsuper.xmobar.overrideAttrs (old: {
-            src = super.fetchFromGitHub {
-              owner  = "jaor";
-              repo   = "xmobar";
-              rev    = "a58e32f7c8af7b03410ab6693019cfc92c9cfca3";
-              sha256 = "186h8qlp7gjdh6r8qns0vfsl40qsi2a1r0w6xzzsy4lvslqhc2fx";
-            };
-            configureFlags = [
-              "-fwith_alsa" "-fwith_conduit" "-fwith_datezone" "-fwith_dbus"
-              "-fwith_inotify" "-fwith_iwlib" "-fwith_mpris"
-              "-fwith_rtsopts" "-fwith_threaded" "-fwith_utf8" "-fwith_uvmeter"
-              "-fwith_weather" "-fwith_xft" "-fwith_xpm"
-            ];
-          })
-        );
-    };
+    overrides = hself: hsuper: hsuper; # No changes currently needed.
   };
 }
