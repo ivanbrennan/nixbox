aa4ecb21d9adac38256a214ab96509e947808862
ivanbrennan <ivan.brennan@gmail.com>
2022-01-29 11:52:52 -0500

    dotinit: don't use stow
    Stow won't work for this, because it detects existing symlinks and
    refuses to overwrite them if they reference a location other than the
    current stow directory. Each time this derivation changes, the current
    stow directory would change.

--- overlays/core/dotinit/default.nix
+++ overlays/core/dotinit/default.nix
@@ -1,17 +1,13 @@
 { runCommandLocal
 , stdenv
-, stow
 }:
 
 runCommandLocal "dotinit" { } ''
   install -D -m755 ${./dotinit} $out/bin/$name
   patchShebangs --host $out/bin
-  substituteInPlace $out/bin/$name \
-      --subst-var-by "out" "$out" \
-      --subst-var-by "stow" "${stow}/bin/stow"
-
+  substituteInPlace $out/bin/$name --subst-var-by "out" "$out"
   ${stdenv.shell} -n $out/bin/$name
 
-  mkdir $out/stow
-  cp -aR ${./stow}/. $out/stow
+  mkdir $out/targets
+  cp -aR ${./targets}/. $out/targets
 ''
