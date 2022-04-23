d0ce325c9c038d25b2dd763dbde0488af6e87264
ivanbrennan <ivan.brennan@gmail.com>
2022-04-23 13:53:09 -0400

    rover: 0.4.8 -> 0.5.1

--- overlays/core/rover/default.nix
+++ overlays/core/rover/default.nix
@@ -6,27 +6,23 @@
 , callPackage
 }:
 
-let
-  rover-fed2 = callPackage ./rover-fed2.nix { };
-in
-  stdenv.mkDerivation rec {
-    pname = "rover";
+stdenv.mkDerivation rec {
+  pname = "rover";
 
-    version = "0.4.8";
+  version = "0.5.1";
 
-    src = fetchurl {
-      url = "https://github.com/apollographql/rover/releases/download/v${version}/rover-v${version}-x86_64-unknown-linux-gnu.tar.gz";
-      sha256 = "sha256-GXKhcePkolzDuMArJDpMjX0W5mpjsU7+3lmzWpJCRcI=";
-    };
+  src = fetchurl {
+    url = "https://github.com/apollographql/rover/releases/download/v${version}/rover-v${version}-x86_64-unknown-linux-gnu.tar.gz";
+    sha256 = "sha256-BJOjL+mlnniQNt2Z+qBA5prO7md2rj19FojVqakhQ9Q=";
+  };
 
-    nativeBuildInputs = [ autoPatchelfHook ];
+  nativeBuildInputs = [ autoPatchelfHook ];
 
-    buildInputs = [ zlib ];
+  buildInputs = [ zlib ];
 
-    sourceRoot = ".";
+  sourceRoot = ".";
 
-    installPhase = ''
-      install -m755 -D dist/rover $out/bin/rover
-      ln -s ${rover-fed2}/bin/rover-fed2 $out/bin/rover-fed2
-    '';
-  }
+  installPhase = ''
+    install -m755 -D dist/rover $out/bin/rover
+  '';
+}
