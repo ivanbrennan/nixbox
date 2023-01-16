66f7bcd12f2fd2da07978e3ca93236caea08d8af
ivanbrennan <ivan.brennan@gmail.com>
2023-01-16 15:49:05 -0500

    neovim: set options

--- overlays/vim/ncore-plugin/lua/ncore/options.lua
+++ overlays/vim/ncore-plugin/lua/ncore/options.lua
@@ -1,64 +1,66 @@
---[[ Defaults as of 9220755302317e8030c5bbf334357c0d64df9fa4
-vim.api.nvim_command('filetype plugin indent on')
-vim.opt.autoindent = true
-vim.opt.autoread = true
-vim.opt.background = "dark"
-vim.opt.backspace = "indent,eol,start"
-vim.opt.belloff = "all"
-vim.opt.compatible = false
-vim.opt.complete = ".,w,b,u,t"
-vim.opt.directory = vim.fn.stdpath('state') .. "/swap//"
-vim.opt.display = "lastline"
-vim.opt.encoding = "utf-8"
-vim.opt.fsync = false
-vim.opt.hidden = true
-vim.opt.history = 10000
-vim.opt.incsearch = true
-vim.opt.joinspaces = false
-vim.opt.langnoremap = true
-vim.opt.langremap = false
-vim.opt.laststatus = 2
-vim.opt.listchars = { tab = "> ", trail = "-", nbsp = "+" }
-vim.opt.mousemodel = "popup_setpos"
-vim.opt.nrformats = "bin,hex"
-vim.opt.ruler = true
-vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,terminal"
-vim.opt.showcmd = true
-vim.opt.smarttab = true
-vim.opt.startofline = false
-vim.opt.switchbuf = "uselast"
-vim.opt.tabpagemax = 50
-vim.opt.timeout = true
-vim.opt.timeoutlen = 1000
-vim.opt.ttyfast = true
-vim.opt.undodir = vim.fn.stdpath('state') .. "/undo//"
-vim.opt.viewoptions = "folds,cursor,curdir"
-vim.opt.wildmenu = true
-vim.opt.wildoptions = "pum,tagfile"
---]]
-
+vim.opt.backup = true
 vim.opt.backupdir = vim.fn.stdpath('state') .. "/backup//"
-vim.opt.fillchars = { eob = " " }
-vim.opt.formatoptions:remove('t') -- don't auto-wrap non-commented text
-vim.opt.formatoptions:append('r') -- auto-comment with Enter
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = false
-vim.opt.wrap = false
-vim.opt.sidescroll = 2
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
 vim.opt.clipboard = 'unnamedplus'
-vim.opt.shortmess:append('I') -- hide the intro message
-vim.opt.tags:remove('./tags')   -- don't just look in the current buffer's directory (vim)
-vim.opt.tags:remove('./tags;')  -- don't just look in the current buffer's directory (nvim)
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.fillchars = { eob = " " }
+vim.opt.foldenable = false
+vim.opt.foldlevelstart = 10
+vim.opt.foldmethod = "syntax"
+vim.opt.formatoptions:append('r')
+vim.opt.formatoptions:remove('t')
+vim.opt.guifont = { "Source Code Pro:h14" }
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.lazyredraw = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "▸ ", trail = "·", extends = "»", precedes = "«" }
+vim.opt.matchtime = 2
+vim.opt.mouse = 'a'
+vim.opt.nrformats = { 'unsigned' }
+vim.opt.number = true
+vim.opt.scrolloff = 1
+vim.opt.shiftwidth = 2
+vim.opt.shortmess:append('I')
+vim.opt.showmatch = true
+vim.opt.showmode = false
+vim.opt.sidescroll = 2
+vim.opt.smartcase = true
+vim.opt.softtabstop = 2
+vim.opt.synmaxcol = 256
+vim.opt.tabstop = 2
 vim.opt.tags:append('./tags;~') -- search for tags recursively upwards until ~
-vim.opt.ttimeoutlen = 5
-
+vim.opt.tags:remove('./tags;')  -- don't just look in the current buffer's directory
 vim.opt.termguicolors = true
+vim.opt.ttimeoutlen = 5
+vim.opt.undofile = true
+vim.opt.wildignore:append({ '*.class' })
+vim.opt.wildignore:append({ '*.gem' })
+vim.opt.wildignore:append({ '*.o' })
+vim.opt.wildignore:append({ '*.obj' })
+vim.opt.wildignore:append({ '*.out' })
+vim.opt.wildignore:append({ '*.rar' })
+vim.opt.wildignore:append({ '*.swp' })
+vim.opt.wildignore:append({ '*.tar.bz2' })
+vim.opt.wildignore:append({ '*.tar.gz' })
+vim.opt.wildignore:append({ '*.tar.xz' })
+vim.opt.wildignore:append({ '*.zip' })
+vim.opt.wildignore:append({ '*/.sass-cache/*' })
+vim.opt.wildignore:append({ '*/.vagrant/*' })
+vim.opt.wildignore:append({ '*/tmp/cache/assets/*/sass/*' })
+vim.opt.wildignore:append({ '*/tmp/cache/assets/*/sprockets/*' })
+vim.opt.wildignore:append({ '*/vendor/cache/*' })
+vim.opt.wildignore:append({ '*~' })
+vim.opt.wildignore:append({ '._*' })
+vim.opt.wildignore:append({ '.svn' })
+vim.opt.wildignore:append({ 'GPATH' })
+vim.opt.wildignore:append({ 'GRTAGS' })
+vim.opt.wildignore:append({ 'GTAGS' })
+vim.opt.wildignorecase = true
+vim.opt.wildmode = { 'longest:full', 'full' }
+vim.opt.wildoptions = { 'pum' }
+vim.opt.wrap = false
+
 vim.cmd('colorscheme wool')
 -- :help nvim_set_hl()
 -- vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#2c2f30', bold = true })
