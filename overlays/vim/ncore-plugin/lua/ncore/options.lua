812eb4098390346e2b2880825b45839bf1e0f276
ivanbrennan <ivan.brennan@gmail.com>
2023-01-16 20:53:54 -0500

    neovim: more options

--- overlays/vim/ncore-plugin/lua/ncore/options.lua
+++ overlays/vim/ncore-plugin/lua/ncore/options.lua
@@ -72,9 +72,42 @@ vim.api.nvim_create_autocmd("FileType", {
   end
 })
 
+local termGroup = vim.api.nvim_create_augroup("TermGroup", {
+  clear = true
+})
+vim.api.nvim_set_hl(0, "EmbeddedTerminal", {
+  bg = "#181818"
+})
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = termGroup,
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.cursorline = false
+    vim.opt_local.winhighlight = 'Normal:EmbeddedTerminal'
+    vim.cmd('startinsert')
+  end
+})
+
+-- Normal colors
+vim.g.terminal_color_0  = '#242e38' -- black
+vim.g.terminal_color_1  = '#de4d3a' -- red
+vim.g.terminal_color_2  = '#b8e068' -- green
+vim.g.terminal_color_3  = '#ebcb8b' -- yellow
+vim.g.terminal_color_4  = '#a1b3c9' -- blue
+vim.g.terminal_color_5  = '#617fa0' -- magenta
+vim.g.terminal_color_6  = '#69d2e7' -- cyan
+vim.g.terminal_color_7  = '#ffffff' -- white
+-- Bright colors
+vim.g.terminal_color_8  = '#303d4b' -- black
+vim.g.terminal_color_9  = '#f4718c' -- red
+vim.g.terminal_color_10 = '#d8e778' -- green
+vim.g.terminal_color_11 = '#f7c352' -- yellow
+vim.g.terminal_color_12 = '#a8d0e0' -- blue
+vim.g.terminal_color_13 = '#53769d' -- magenta
+vim.g.terminal_color_14 = '#26a6a6' -- cyan
+vim.g.terminal_color_15 = '#ffffff' -- white
+
 vim.cmd('colorscheme wool')
--- :help nvim_set_hl()
--- vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#2c2f30', bold = true })
 
 vim.cmd.aunmenu({ 'PopUp.How-to\\ disable\\ mouse' })
 vim.cmd.aunmenu({ 'PopUp.-1-' })
