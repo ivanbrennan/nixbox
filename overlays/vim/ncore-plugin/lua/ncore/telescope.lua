9cb571161cd06ee4e21ea89bfef633f09d5eadeb
ivanbrennan <ivan.brennan@gmail.com>
2023-02-05 16:18:58 -0500

    telescope file_browser

--- overlays/vim/ncore-plugin/lua/ncore/telescope.lua
+++ overlays/vim/ncore-plugin/lua/ncore/telescope.lua
@@ -59,4 +59,21 @@ telescope.setup({
       },
     },
   },
+  extensions = {
+    fzf = {
+      fuzzy = false,                  -- exact matching, space separated AND, | OR
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true,    -- override the file sorter
+      case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                                      -- the default case_mode is "smart_case"
+    },
+    file_browser = {
+      theme = "ivy",
+      hijack_netrw = false, -- dirvish still takes precedence
+      dir_icon = '',
+    },
+  },
 })
+
+require('telescope').load_extension('fzf')
+require('telescope').load_extension('file_browser')
