33d20c12d01e959fe4ae7f29c055ec35b1c412d5
ivanbrennan <ivan.brennan@gmail.com>
2023-02-07 21:35:35 -0500

    refactor neovim-runtime-attrs

--- overlays/vim/default.nix
+++ overlays/vim/default.nix
@@ -10,21 +10,25 @@ let
 
   # TODO: Remove etc/ prefix once we have the following fix:
   # https://github.com/NixOS/nixpkgs/pull/209755
-  neovim-runtime-attrs = {
-    "etc/ftdetect/bats.lua".source = ./runtime/ftdetect/bats.lua;
-    "etc/ftdetect/geojson.lua".source = ./runtime/ftdetect/geojson.lua;
-    "etc/ftdetect/mutt.lua".source = ./runtime/ftdetect/mutt.lua;
-    "etc/ftdetect/ssh.lua".source = ./runtime/ftdetect/ssh.lua;
-    "etc/ftplugin/asm.lua".source = ./runtime/ftplugin/asm.lua;
-    "etc/ftplugin/c.lua".source = ./runtime/ftplugin/c.lua;
-    "etc/ftplugin/cpp.lua".source = ./runtime/ftplugin/cpp.lua;
-    "etc/ftplugin/help.lua".source = ./runtime/ftplugin/help.lua;
-    "etc/indent/c.lua".source = ./runtime/indent/c.lua;
-    "etc/indent/cpp.lua".source = ./runtime/indent/cpp.lua;
-    "etc/indent/java.lua".source = ./runtime/indent/java.lua;
-    "etc/indent/make.lua".source = ./runtime/indent/make.lua;
-    "etc/indent/sh.lua".source = ./runtime/indent/sh.lua;
+  runtime-attr = str: {
+    "etc/${str}".source = ./runtime + ("/" + str);
   };
+  neovim-runtime-attrs =
+    builtins.foldl' (attrs: x: attrs // runtime-attr x) {} [
+      "ftdetect/bats.lua"
+      "ftdetect/geojson.lua"
+      "ftdetect/mutt.lua"
+      "ftdetect/ssh.lua"
+      "ftplugin/asm.lua"
+      "ftplugin/c.lua"
+      "ftplugin/cpp.lua"
+      "ftplugin/help.lua"
+      "indent/c.lua"
+      "indent/cpp.lua"
+      "indent/java.lua"
+      "indent/make.lua"
+      "indent/sh.lua"
+    ];
 
   # NOTE: Based on nixos/modules/programs/neovim.nix
   neovim-runtime = super.linkFarm "neovim-runtime" (
