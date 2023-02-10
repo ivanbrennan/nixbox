515d0221e3665a38ee0eadb652dce34b77797ddf
ivanbrennan <ivan.brennan@gmail.com>
2023-02-10 02:24:00 -0500

    neovim: dirvish update

--- overlays/vim/runtime/ftplugin/dirvish.lua
+++ overlays/vim/runtime/ftplugin/dirvish.lua
@@ -1,10 +1,10 @@
-b = vim.b
-cmd = vim.cmd
-fn = vim.fn
-set = vim.keymap.set
+local b = vim.b
+local cmd = vim.cmd
+local fn = vim.fn
+local set = vim.keymap.set
 
 -- reset this even if reloading
-b.show_hidden_files = 1
+b.showing_hidden_files = 1
 
 -- finish here if reloading
 if b.loaded_user_ftplugin == 1 then
@@ -22,16 +22,32 @@ set('n', 'q', '<Plug>(dirvish_quit)', {
   nowait = true,
 })
 
-set('n', 'h', function()
-  local cursor_position = fn.getcurpos()
+-- For some reason, conceallevel=3 only takes effect if we run these commands
+-- in-line, rather than performing them within a function.
+local conceal_script = table.concat({
+  ':let b:saved_position=getcurpos()<Bar>',
+  [[silent keeppatterns g@\v/\.[^\/]+/?$@d _<CR>]],
+  ':setlocal conceallevel=3<Bar>',
+  'let b:showing_hidden_files=0<Bar>',
+  'call setpos(".", b:saved_position)<Bar>',
+  'unlet b:saved_position<CR>',
+})
+set('n', '<Plug>(conceal_hidden_files)', conceal_script, {
+  buffer = true,
+  silent = true,
+})
 
-  if b.show_hidden_files == 1 then
-    cmd([[keeppatterns g@\v/\.[^\/]+/?$@d]])
-    b.show_hidden_files = 0
-  else
-    cmd.Dirvish('%')
-    b.show_hidden_files = 1
-  end
-
-  fn.setpos('.', cursor_position)
+set('n', '<Plug>(reveal_hidden_files)', function()
+  local saved_position = fn.getcurpos()
+  cmd.Dirvish()
+  b.showing_hidden_files = 1
+  fn.setpos(".", saved_position)
 end, { buffer = true, silent = true })
+
+set('n', 'h', function()
+  if b.showing_hidden_files == 1 then
+    return '<Plug>(conceal_hidden_files)'
+  else
+    return '<Plug>(reveal_hidden_files)'
+  end
+end, { buffer = true, expr = true })
