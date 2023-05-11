03ab130fd3ff6f288f04796a562d3f794c3eabe7
ivanbrennan <ivan.brennan@gmail.com>
2023-05-11 14:21:11 -0400

    udisks_dmenu: handle encrypted drives

--- overlays/core/udisks_dmenu/default.nix
+++ overlays/core/udisks_dmenu/default.nix
@@ -1,7 +1,10 @@
 { coreutils
 , dmenu
 , gawk
+, gnused
+, jq
 , libnotify
+, pinentry-gtk2
 , runCommandLocal
 , stdenv
 , udisks2
@@ -11,12 +14,17 @@
 runCommandLocal "udisks_dmenu" { } ''
   install -D -m755 ${./udisks_dmenu} $out/bin/$name
   patchShebangs --host $out/bin
-  substituteInPlace $out/bin/$name                           \
-      --replace "awk"         "${gawk}/bin/gawk"             \
-      --replace "dmenu"       "${dmenu}/bin/dmenu"           \
-      --replace "lsblk"       "${util-linux}/bin/lsblk"      \
-      --replace "notify-send" "${libnotify}/bin/notify-send" \
-      --replace "udisks"      "${udisks2}/bin/udisks"
+  substituteInPlace $out/bin/$name                                 \
+      --subst-var-by "awk"         "${gawk}/bin/gawk"              \
+      --subst-var-by "dmenu"       "${dmenu}/bin/dmenu"            \
+      --subst-var-by "jq"          "${jq}/bin/jq"                  \
+      --subst-var-by "lsblk"       "${util-linux}/bin/lsblk"       \
+      --subst-var-by "notify_send" "${libnotify}/bin/notify-send"  \
+      --subst-var-by "pinentry"    "${pinentry-gtk2}/bin/pinentry" \
+      --subst-var-by "sed"         "${gnused}/bin/sed"             \
+      --subst-var-by "sort"        "${coreutils}/bin/sort"         \
+      --subst-var-by "tr"          "${coreutils}/bin/tr"           \
+      --subst-var-by "udisksctl"   "${udisks2}/bin/udisksctl"
 
   ${stdenv.shell} -n $out/bin/$name
 ''
