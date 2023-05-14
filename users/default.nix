029e4bd79c2e7934b8c1d880eb237a9495f62897
ivanbrennan <ivan.brennan@gmail.com>
2023-05-14 09:27:34 -0400

    update hashedPassword
    I generate the updated hash by running:
    
      mkpasswd
    
    This is in contrast to the way I generated it previously (during
    install):
    
      mkpasswd -m sha-512
    
    An updated hash could also be generated by specifying the yescrypt
    method explicitly,
    
      mkpasswd -m yescrypt
    
    but that's the default now.
    
    NixOS 22.11 release notes state the following:
    
    > * Software that uses the crypt password hashing API is now using the
    >   implementation provided by libxcrypt instead of glibc’s, which enables
    >   support for more secure algorithms.
    >
    >   * Support for algorithms that libxcrypt does not consider strong are
    >     deprecated as of this release, and will be removed in NixOS 23.05.
    >
    >     * This includes system login passwords. Given this, we strongly
    >       encourage all users to update their system passwords, as you will
    >       be unable to login if password hashes are not migrated by the time
    >       their support is removed.
    >
    >       * When using users.users.<name>.hashedPassword to configure user
    >         passwords, run mkpasswd, and use the yescrypt hash that is provided as
    >         the new value.
    
    https://nixos.org/manual/nixos/stable/release-notes.html#sec-release-22.11-highlights

--- users/default.nix
+++ users/default.nix
@@ -16,7 +16,7 @@
         "wheel"
         "wireshark"
       ];
-      hashedPassword = "$6$4uOYQEuFA$RqNmGNfQcR6mPK2.jSHPntF43HgN6BJP4nwQANNUbp8ulpquniQNqecgUMVGRBsjBzt2b7gJBhCCedbUYmI/60";
+      hashedPassword = "$y$j9T$vowJiSYMEds55yVoU3Rwn0$tdRX1zSuZRd8I.NJJwzYVEGKQn.ATGPXmH/ZubzmbE4";
       openssh.authorizedKeys.keys = [
         ''
           from="192.168.0.0/24" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDABjGqcHsTTDjmT30YUZ9VUJMz0cNYFqIRROz/7NVmS79gvIeS4/ll+flOtdgVcsDijjghHqA9AM/4OCv5sKICaufRV73PS4HKk06yfiCS2au5YzIg/jd+7gK5smxpS+55qtR0Yu1hOBrBik0Q2J7biLNpXLqHLnnrrrS5mkgnIRAb7Ojv/CQKT+ZDcusJWsZ7pzxY1BHqC59VNuy79knVbPAE44n6jnIXlfcIACVqmHlU/W6KVvxfkv+lncf2t6SAj3AuWdFD98YuWxN5QlGBPe+If5WwneYUc3ENjiSAJu1sHUYU9BMhe9YEFiCZVzKsv45Lr+1HlA225u447835 ivan.brennan@gmail.com
