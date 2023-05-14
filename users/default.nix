4c314a1dab423d36c5800d87522cce9451123514
ivanbrennan <ivan.brennan@gmail.com>
2023-05-14 11:16:14 -0400

    update authorized keys

--- users/default.nix
+++ users/default.nix
@@ -21,6 +21,9 @@
         ''
           from="192.168.0.0/24" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDABjGqcHsTTDjmT30YUZ9VUJMz0cNYFqIRROz/7NVmS79gvIeS4/ll+flOtdgVcsDijjghHqA9AM/4OCv5sKICaufRV73PS4HKk06yfiCS2au5YzIg/jd+7gK5smxpS+55qtR0Yu1hOBrBik0Q2J7biLNpXLqHLnnrrrS5mkgnIRAb7Ojv/CQKT+ZDcusJWsZ7pzxY1BHqC59VNuy79knVbPAE44n6jnIXlfcIACVqmHlU/W6KVvxfkv+lncf2t6SAj3AuWdFD98YuWxN5QlGBPe+If5WwneYUc3ENjiSAJu1sHUYU9BMhe9YEFiCZVzKsv45Lr+1HlA225u447835 ivan.brennan@gmail.com
         ''
+        ''
+          from="192.168.0.0/24" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBuGpfDRp1CJ/xrSBnSq2SBmx7KzvuIPELaCnrnOdeXp ivan@thinkpad6
+        ''
       ];
     };
 
