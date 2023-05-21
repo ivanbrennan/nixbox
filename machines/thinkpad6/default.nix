534d4df57e70c3fa7d7a881fd2f78617701590b8
ivanbrennan <ivan.brennan@gmail.com>
2023-05-21 05:29:51 +0000

    machines/thinkpad6: edit

---
+++ machines/thinkpad6/default.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      # ( let
+      #     nixos-hardware = builtins.fetchTarball {
+      #       url = "https://github.com/NixOS/nixos-hardware/archive/71ce85372a614d418d5e303dd5702a79d1545c04.tar.gz";
+      #       sha256 = "0000000000000000000000000000000000000000000000000000";
+      #     };
+      #   in "${nixos-hardware}/lenovo/thinkpad/x1/6th-gen"
+      # )
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/disk/by-uuid/ef0fbbb1-f41f-4ae3-9139-1a81b12eb06a";
+      preLVM = true;
+    };
+  };
+
+  networking.hostName = "thinkpad6"; # Define your hostname.
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+}
+
--- machines/thinkpad6/hardware-configuration.nix
+++ machines/thinkpad6/hardware-configuration.nix
@@ -27,14 +27,6 @@
     [ { device = "/dev/disk/by-uuid/078f7f2d-0915-42c9-910a-2acabc1db816"; }
     ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
