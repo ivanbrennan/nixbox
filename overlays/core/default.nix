25a3d39c54185006dcec73a0b188b579b6d3fdd2
ivanbrennan <ivan.brennan@gmail.com>
2023-05-25 21:37:01 -0400

    helm: default to 3
    Install the latest version of helm globally. If/when helm 2 is needed,
    it can be run in a nix-shell, e.g.
    
      nix-shell -p kubernetes-helm-2

--- overlays/core/default.nix
+++ overlays/core/default.nix
@@ -58,7 +58,8 @@ in self: super: {
 
   interactive-editor = super.callPackage ./interactive-editor { };
 
-  kubernetes-helm =
+  # v2.11.0
+  kubernetes-helm-2 =
     (pinned {
       url = "https://github.com/NixOS/nixpkgs/archive/a071bfa7e7bbd62e1b43830e5e79d8b36afe5fa6.tar.gz";
       sha256 = "0yl2bsan5x69a7z6r1fb8zlv4zpibq67pb2x45jjp7wx42ssdkq2";
