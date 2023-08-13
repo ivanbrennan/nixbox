230797d50e5d31c5e211ab8b3d86fff3b610ad17
ivanbrennan <ivan.brennan@gmail.com>
2023-08-13 00:12:51 -0400

    Revert "sops-nix, agenix: thinkpad6"
    This reverts commit 8404f70261af5d77f92cfe3e759144b9c08ee78f.
    
    Manually editing .sops.yaml was insufficient, because we need to also
    rekey the secrets.

--- agenix/secrets/secrets.nix
+++ agenix/secrets/secrets.nix
@@ -1,9 +1,8 @@
 let
   ivan = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7BzvEYjobE2jwwdpJVZhqZyj6fGMuHf51IKRn05eyG ivan@agenix";
-  thinkpad6 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINmp90KAZvG/C/J1/wmuMFsI5uju1v2fRQiGDguYupli root@nixos"
   thinkpad9 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINb6F08Gc0KSiEYKZcmlnS8bNNJEqTnmdTj0QlvMV07a root@nixos";
   users = [ ivan ];
-  systems = [ thinkpad6 thinkpad9 ];
+  systems = [ thinkpad9 ];
 in
 {
   "secret1.age".publicKeys = users ++ systems;
