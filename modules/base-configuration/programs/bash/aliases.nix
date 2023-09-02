1a3ef54ad60141f6d4d0e47dd5f826499d88aa83
ivanbrennan <ivan.brennan@gmail.com>
2023-09-02 18:33:56 -0400

    fix aliases
    It doesn't make sense to point aliases at executables under specific
    store paths, because it means rebuilding (nixos-rebuild) without logging
    out and back in leaves you with stale aliases. Since I'm only aliasing
    common untilities, all of which are installed in the system, let aliases
    resolve them by normal means ($PATH).

--- modules/base-configuration/programs/bash/aliases.nix
+++ modules/base-configuration/programs/bash/aliases.nix
@@ -1,36 +1,25 @@
 { pkgs, ... }:
 
-let
-  du = "${pkgs.coreutils}/bin/du";
-  find = "${pkgs.findutils}/bin/find";
-  git = "${pkgs.git}/bin/git";
-  grep = "${pkgs.gnugrep}/bin/grep";
-  ls = "${pkgs.coreutils}/bin/ls";
-  nix-shell = "${pkgs.nix}/bin/nix-shell";
-  tmux = "${pkgs.tmux}/bin/tmux";
-  xdg-open = "${pkgs.xdg_utils}/bin/xdg-open";
-in
-
 {
   SS = "unset __ETC_BASHRC_SOURCED && . /etc/bashrc";
-  du1 = "${du} -h -d 1";
+  du1 = "du -h -d 1";
   fgg = "fg '%-'";
-  finame = "${find} . -name";
+  finame = "find . -name";
   git-default-branch =
-    "${git} rev-parse 2>/dev/null && " +
-    "${git} branch --list main master | ${grep} -m1 -oP 'main|master'";
-  gpc = "${git} log $(git-default-branch)..HEAD -- | wc -l";
-  gpp = "${git} log $(git-default-branch)..HEAD --reverse -p";
-  gpr = "${git} log $(git-default-branch)..HEAD --reverse";
-  grep = "${grep} --color=auto";
-  gst = "${git} status";
-  ls = "${ls} --color=tty";
+    "git rev-parse 2>/dev/null && " +
+    "git branch --list main master | grep -m1 -oP 'main|master'";
+  gpc = "git log $(git-default-branch)..HEAD -- | wc -l";
+  gpp = "git log $(git-default-branch)..HEAD --reverse -p";
+  gpr = "git log $(git-default-branch)..HEAD --reverse";
+  grep = "grep --color=auto";
+  gst = "git status";
+  ls = "ls --color=tty";
   l = "ls -Alh";
   la = "ls -A";
   ll = "ls -l";
   lsd = "ls -d .[!.]*";
-  nirb = "${nix-shell} -p ruby interactive-editor --command irb";
-  t = tmux;
-  tls = "${tmux} ls";
-  xopen = xdg-open;
+  nirb = "nix-shell -p ruby interactive-editor --command irb";
+  t = "tmux";
+  tls = "tmux ls";
+  xopen = "xdg-open";
 }
