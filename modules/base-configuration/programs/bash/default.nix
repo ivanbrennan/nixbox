c6bba215d2a49c6669111cb0df6baa2373de418a
ivanbrennan <ivan.brennan@gmail.com>
2024-07-07 22:56:35 -0400

    doc: pass-otp completion workaround
    Reference a discussion that lends additional context and mentions a
    better solution that could be applied upstream.

--- modules/base-configuration/programs/bash/default.nix
+++ modules/base-configuration/programs/bash/default.nix
@@ -9,6 +9,8 @@
     . ${pkgs.fzf}/share/fzf/key-bindings.bash
     . ${pkgs.etcdots}/share/etcdots/key-bindings.bash
     . ${pkgs.git}/share/bash-completion/completions/git-prompt.sh
+
+    # https://github.com/tadfisher/pass-otp/issues/137#issuecomment-1942254432
     . ${pkgs.pass.extensions.pass-otp}/share/bash-completion/completions/pass-otp
   '';
 }
