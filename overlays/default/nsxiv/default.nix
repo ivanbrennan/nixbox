cb5fc0b1b433ec39735abd57c9be125f2d728416
ivanbrennan <ivan.brennan@gmail.com>
2024-07-09 22:51:25 -0400

    nsxiv-rifle
    Inspired by nsxiv-rifle:
    https://codeberg.org/nsxiv/nsxiv-extra/src/commit/f7d1efe3495949e2e88fdfef37aed5a40400acea/scripts/nsxiv-rifle

--- overlays/default/nsxiv/default.nix
+++ overlays/default/nsxiv/default.nix
@@ -1,7 +1,11 @@
-{ nsxiv,
-  runCommandLocal,
-  symlinkJoin,
-  writeShellScriptBin
+{ coreutils
+, findutils
+, gnugrep
+, nsxiv
+, runCommandLocal
+, stdenv
+, symlinkJoin
+, writeShellScriptBin
 }:
 
 let
@@ -9,20 +13,33 @@ let
     conf = builtins.readFile ./config.h;
   };
 
+  nsxiv-unwrapped = runCommandLocal "nsxiv-unwrapped" { } ''
+    install -D -m755 ${configured}/bin/nsxiv $out/bin/nsxiv-unwrapped
+  '';
+
   nsxiv-wrapped = writeShellScriptBin "nsxiv" ''
     NSXIV_OPTS=''${NSXIV_OPTS:-"--animate --no-bar"}
     exec ${configured}/bin/nsxiv $NSXIV_OPTS "$@"
   '';
 
-  nsxiv-unwrapped = runCommandLocal "nsxiv-unwrapped" { } ''
-    install -D -m755 ${configured}/bin/nsxiv $out/bin/nsxiv-unwrapped
+  nsxiv-rifle = runCommandLocal "nsxiv-rifle" { } ''
+    install -D -m755 ${./nsxiv-rifle} $out/bin/$name
+    patchShebangs --host $out/bin
+    substituteInPlace $out/bin/$name                        \
+        --subst-var-by "grep"  "${gnugrep}/bin/grep"        \
+        --subst-var-by "find"  "${findutils}/bin/find"      \
+        --subst-var-by "nsxiv" "${nsxiv-wrapped}/bin/nsxiv" \
+        --subst-var-by "sort"  "${coreutils}/bin/sort"      \
+        --subst-var-by "tee"   "${coreutils}/bin/tee"
+    ${stdenv.shell} -n $out/bin/$name
   '';
 
 in symlinkJoin {
   name = "nsxiv";
   paths = [
-    nsxiv-wrapped
     nsxiv-unwrapped
+    nsxiv-wrapped
+    nsxiv-rifle
     nsxiv.man
     nsxiv.doc
   ];
